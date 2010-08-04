3dd68fd4001e916368ede349a78445428ded7805
alexey.petrov.nnov <alexey.petrov.nnov@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-08-04 15:15:23 +0000

    adding notification features to this prototype

--- cloud-agent/trunk/clean_rackspace.py
+++ cloud-agent/trunk/clean_rackspace.py
@@ -14,12 +14,16 @@ RACKSPACE_KEY = os.getenv( "RACKSPACE_KEY" )
 
 #--------------------------------------------------------------------------------------
 import cloudfiles
-
+print "-------------- CloudFiles --------------"
 a_cloudfiles_conn = cloudfiles.get_connection( RACKSPACE_USER, RACKSPACE_KEY, timeout = 500 )
 for a_container_name in a_cloudfiles_conn.list_containers() :
+    print a_container_name
     a_cloudfiles_container = a_cloudfiles_conn[ a_container_name ]
     an_objects = a_cloudfiles_container.get_objects()
-    [ a_cloudfiles_container.delete_object( an_object ) for an_object in an_objects ]
+    for an_object in an_objects :
+        a_cloudfiles_container.delete_object( an_object )
+        print an_object
+        pass
     a_cloudfiles_conn.delete_container( a_cloudfiles_container ) 
     pass
 
@@ -27,10 +31,13 @@ for a_container_name in a_cloudfiles_conn.list_containers() :
 #--------------------------------------------------------------------------------------
 from libcloud.types import Provider 
 from libcloud.providers import get_driver 
-
+print "-------------- CloudServers --------------"
 Driver = get_driver( Provider.RACKSPACE ) 
 a_libcloud_conn = Driver( RACKSPACE_USER, RACKSPACE_KEY ) 
-[ node.destroy() for node in a_libcloud_conn.list_nodes() ]
+for a_node in a_libcloud_conn.list_nodes() :
+    a_node.destroy()
+    print a_node.name
+    pass
 
 
 #--------------------------------------------------------------------------------------
