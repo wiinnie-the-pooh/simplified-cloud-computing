72973a0f68df0fc893d17084461aa3bf96b06b9c
alexey.petrov.nnov <alexey.petrov.nnov@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-08-04 13:23:56 +0000

    the first workable release

---
+++ cloud-agent/tags/r0.2/clean_rackspace.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+
+#--------------------------------------------------------------------------------------
+"""
+Cleans all nodes from cloudservers and cloudfiles that correspond to defined rackspace account
+"""
+
+#--------------------------------------------------------------------------------------
+import os
+RACKSPACE_USER = os.getenv( "RACKSPACE_USER" )
+RACKSPACE_KEY = os.getenv( "RACKSPACE_KEY" )
+
+
+#--------------------------------------------------------------------------------------
+import cloudfiles
+
+a_cloudfiles_conn = cloudfiles.get_connection( RACKSPACE_USER, RACKSPACE_KEY, timeout = 500 )
+for a_container_name in a_cloudfiles_conn.list_containers() :
+    a_cloudfiles_container = a_cloudfiles_conn[ a_container_name ]
+    an_objects = a_cloudfiles_container.get_objects()
+    [ a_cloudfiles_container.delete_object( an_object ) for an_object in an_objects ]
+    a_cloudfiles_conn.delete_container( a_cloudfiles_container ) 
+    pass
+
+
+#--------------------------------------------------------------------------------------
+from libcloud.types import Provider 
+from libcloud.providers import get_driver 
+
+Driver = get_driver( Provider.RACKSPACE ) 
+a_libcloud_conn = Driver( RACKSPACE_USER, RACKSPACE_KEY ) 
+[ node.destroy() for node in a_libcloud_conn.list_nodes() ]
+
+
+#--------------------------------------------------------------------------------------
+print "OK"
+
+
+#--------------------------------------------------------------------------------------
