1cb6a88001d9e85453a090dd70abbe01c71e2c4b
alexey.petrov.nnov <alexey.petrov.nnov@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-08-18 14:32:36 +0000

    starting implementation of "cloud data transfer" protocol

---
+++ cloud-agent/trunk/amazon_upload.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+#--------------------------------------------------------------------------------------
+## Copyright 2010 Alexey Petrov
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+
+#--------------------------------------------------------------------------------------
+"""
+This script is responsible for efficient uploading of multi source data
+"""
+
+
+#--------------------------------------------------------------------------------------
+import balloon.common as common
+from balloon.common import print_d, print_e, sh_command, ssh_command, Timer
+
+import balloon.amazon as amazon
+
+import sys, os, os.path, uuid
+
+
+#--------------------------------------------------------------------------------------
+# Defining utility command-line interface
+
+an_usage_description = "%prog"
+an_usage_description += common.add_usage_description()
+an_usage_description += amazon.add_usage_description()
+an_usage_description += " <source 1> <source 2> ..."
+
+from optparse import IndentedHelpFormatter
+a_help_formatter = IndentedHelpFormatter( width = 127 )
+
+from optparse import OptionParser
+a_option_parser = OptionParser( usage = an_usage_description, version="%prog 0.1", formatter = a_help_formatter )
+
+# Definition of the command line arguments
+common.add_parser_options( a_option_parser )
+
+amazon.add_parser_options( a_option_parser )
+    
+an_engine_dir = os.path.abspath( os.path.dirname( sys.argv[ 0 ] ) )
+
+
+#--------------------------------------------------------------------------------------
+# Extracting and verifying command-line arguments
+
+an_options, an_args = a_option_parser.parse_args()
+
+common.extract_options( an_options )
+
+a_sources = list()
+for an_arg in an_args :
+    if not os.path.exists( an_arg ) :
+        print_e( "The given source should exists\n" )
+        pass
+    a_sources.append( os.path.abspath( an_arg ) )
+    pass
+
+print_d( "a_sources = %r\n" % a_sources )
+
+AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY = amazon.extract_options( an_options )
+
+
+print_d( "\n---------------------------------------------------------------------------\n" )
+print_d( 'OK\n' )
+
+
+#--------------------------------------------------------------------------------------
