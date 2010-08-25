3b81bd015574dfb4bfc364ef87d657f1e2483225
alexey.petrov.nnov <alexey.petrov.nnov@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-08-25 10:01:01 +0000

    the first workable release

---
+++ cloud-agent/tags/r0.11/amazon_threaded_download.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+
+#--------------------------------------------------------------------------------------
+## Copyright 2010 Alexey Petrov
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+
+#------------------------------------------------------------------------------------------
+"""
+This script is responsible for efficient downloading of multi file data
+"""
+
+
+#------------------------------------------------------------------------------------------
+import balloon.common as common
+from balloon.common import print_d, init_printing, print_i, print_e, sh_command, ssh_command, Timer, Worker
+
+import balloon.amazon as amazon
+
+import boto
+from boto.s3.key import Key
+
+import sys, os, os.path, uuid, hashlib
+
+
+#------------------------------------------------------------------------------------------
+def download_item( the_item_key, the_working_dir, the_printing_depth ) :
+    a_file_path = os.path.join( the_working_dir, the_item_key.name )
+    print_d( "a_file_path = %s\n" % a_file_path, the_printing_depth )
+
+    the_item_key.get_contents_to_filename( a_file_path )
+    print_d( "an_item_key = %s\n" % the_item_key, the_printing_depth )
+        
+    pass
+
+
+#------------------------------------------------------------------------------------------
+class DownloadItem :
+    def __init__( self, the_item_key, the_working_dir, the_printing_depth ) :
+        self.item_key = the_item_key
+        self.working_dir = the_working_dir
+        self.printing_depth = the_printing_depth
+
+        pass
+    
+    def run( self ) :
+        download_item( self.item_key, self.working_dir, self.printing_depth )
+
+        return self
+
+    pass
+
+
+#------------------------------------------------------------------------------------------
+def download_items( the_worker, the_number_threads, the_file_bucket, the_file_basename, the_output_dir, the_printing_depth ) :
+    a_worker = Worker( the_number_threads )
+
+    import tempfile
+    a_working_dir = tempfile.mkdtemp( dir = the_output_dir )
+    a_working_dir = os.path.join( the_output_dir, a_working_dir )
+    print_d( "a_working_dir = '%s'\n" % a_working_dir, the_printing_depth )
+
+    for an_item_key in the_file_bucket.get_all_keys() :
+        a_task = DownloadItem( an_item_key, a_working_dir, the_printing_depth + 1 )
+        
+        a_worker.put( a_task )
+        
+        pass
+
+    a_worker.join()
+
+    the_worker.status = a_worker.status
+
+    sh_command( "cd '%s' && cat %s.tgz-* | tar -xzf - -C '%s'" % 
+                ( a_working_dir, the_file_basename, the_output_dir ), 
+                the_printing_depth )
+
+    import shutil
+    shutil.rmtree( a_working_dir, True )
+    
+    pass
+
+
+#------------------------------------------------------------------------------------------
+def download_file( the_worker, the_number_threads, the_s3_conn, the_study_file_key, the_study_id, the_output_dir, the_printing_depth ) :
+    a_file_name = the_study_file_key.key.split( ':' )[ 0 ]
+    a_file_dirname = os.path.dirname( a_file_name )
+    a_file_basename = os.path.basename( a_file_name )
+
+    print_d( "the_study_file_key = %s\n" % the_study_file_key, the_printing_depth )
+
+    a_file_id = '%s/%s' % ( the_study_id, the_study_file_key.key )
+    print_d( "a_file_id = '%s'\n" % a_file_id, the_printing_depth )
+
+    a_file_bucket_name = hashlib.md5( a_file_id ).hexdigest()
+
+    a_file_bucket = the_s3_conn.get_bucket( a_file_bucket_name )
+    print_d( "a_file_bucket = %s\n" % a_file_bucket, the_printing_depth )
+
+    download_items( the_worker, the_number_threads, a_file_bucket, a_file_basename, the_output_dir, the_printing_depth + 1 )
+        
+    pass
+
+
+#------------------------------------------------------------------------------------------
+class DownloadFile :
+    def __init__( self, the_worker, the_number_threads, the_s3_conn, the_study_file_key, the_study_id, the_output_dir, the_printing_depth ) :
+        self.worker = the_worker
+        self.number_threads = the_number_threads
+        self.s3_conn = the_s3_conn
+        self.study_file_key = the_study_file_key
+        self.study_id = the_study_id
+        self.output_dir = the_output_dir
+        self.printing_depth = the_printing_depth
+
+        pass
+    
+    def run( self ) :
+        download_file( self.worker, self.number_threads, self.s3_conn, self.study_file_key, self.study_id, self.output_dir, self.printing_depth )
+
+        return self
+
+    pass
+
+
+#------------------------------------------------------------------------------------------
+def download_files( the_worker, the_number_threads, the_s3_conn, the_study_file_keys, the_study_id, the_output_dir, the_printing_depth ) :
+    for a_study_file_key in the_study_file_keys :
+        a_task = DownloadFile( the_worker, the_number_threads, the_s3_conn, a_study_file_key, the_study_id, the_output_dir, the_printing_depth )
+        
+        the_worker.put( a_task )
+        
+        pass
+
+    the_worker.join()
+
+    pass
+
+
+#------------------------------------------------------------------------------------------
+# Defining utility command-line interface
+
+an_usage_description = "%prog --study-name='my uploaded study' --output-dir='./tmp'"
+an_usage_description += common.add_usage_description()
+an_usage_description += amazon.add_usage_description()
+an_usage_description += amazon.add_timeout_usage_description()
+an_usage_description += amazon.add_threading_usage_description()
+
+from optparse import IndentedHelpFormatter
+a_help_formatter = IndentedHelpFormatter( width = 127 )
+
+from optparse import OptionParser
+a_option_parser = OptionParser( usage = an_usage_description, version="%prog 0.1", formatter = a_help_formatter )
+
+# Definition of the command line arguments
+a_option_parser.add_option( "--study-name",
+                            metavar = "< an unique name of the user study >",
+                            action = "store",
+                            dest = "study_name",
+                            help = "(intialized from input, otherwise)" )
+a_option_parser.add_option( "--output-dir",
+                            metavar = "< location of the task defintion >",
+                            action = "store",
+                            dest = "output_dir",
+                            help = "(the same a 'study' name, by default)" )
+common.add_parser_options( a_option_parser )
+amazon.add_parser_options( a_option_parser )
+amazon.add_timeout_options( a_option_parser )
+amazon.add_threading_parser_options( a_option_parser )
+    
+an_engine_dir = os.path.abspath( os.path.dirname( sys.argv[ 0 ] ) )
+
+
+#------------------------------------------------------------------------------------------
+# Extracting and verifying command-line arguments
+
+an_options, an_args = a_option_parser.parse_args()
+
+common.extract_options( an_options )
+
+a_study_name = an_options.study_name
+if a_study_name == None :
+    a_study_name = raw_input()
+    pass
+
+print_d( "a_study_name = '%s'\n" % a_study_name )
+    
+an_output_dir = an_options.output_dir
+if an_output_dir == None :
+    an_output_dir = os.path.join( an_engine_dir, a_study_name )
+    pass
+
+import shutil
+shutil.rmtree( an_output_dir, True )
+os.makedirs( an_output_dir )
+
+print_d( "an_output_dir = '%s'\n" % an_output_dir )
+
+AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY = amazon.extract_options( an_options )
+
+a_number_threads = amazon.extract_threading_options( an_options, a_option_parser )
+
+amazon.extract_timeout_options( an_options, a_option_parser )
+
+
+print_i( "--------------------------- Connecting to Amazon S3 -----------------------------\n" )
+a_s3_conn = boto.connect_s3( AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY )
+print_d( "a_s3_conn = %r\n" % a_s3_conn )
+
+a_canonical_user_id = a_s3_conn.get_canonical_user_id()
+print_d( "a_canonical_user_id = '%s'\n" % a_canonical_user_id )
+
+
+print_i( "------------------------ Looking for the study bucket ---------------------------\n" )
+a_study_id = '%s/%s' % ( a_canonical_user_id, a_study_name )
+a_study_bucket_name = hashlib.md5( a_study_id ).hexdigest()
+
+a_study_bucket = None
+try :
+    a_study_bucket = a_s3_conn.get_bucket( a_study_bucket_name )
+except :
+    print_e( "There is no study with such name ('%s')\n" % a_study_name )
+    pass
+
+print_d( "a_study_bucket = '%s'\n" % a_study_bucket.name )
+
+
+print_i( "--------------------------- Reading the study files -----------------------------\n" )
+a_data_loading_time = Timer()
+
+a_study_file_keys = a_study_bucket.get_all_keys()
+a_worker = Worker( len( a_study_file_keys ) )
+
+download_files( a_worker, a_number_threads, a_s3_conn, a_study_file_keys, a_study_id, an_output_dir, 0 )
+
+print_d( "a_data_loading_time = %s, sec\n" % a_data_loading_time )
+
+
+print_i( "-------------------------------------- %s ---------------------------------------\n" % a_worker.status )
+
---
+++ cloud-agent/tags/r0.11/balloon/amazon/__init__.py
@@ -0,0 +1,223 @@
+
+
+#--------------------------------------------------------------------------------------
+## Copyright 2010 Alexey Petrov
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+
+#--------------------------------------------------------------------------------------
+from balloon.common import print_e, print_d, ssh_command
+
+import boto
+from boto.s3.key import Key
+
+import os, os.path
+
+
+#--------------------------------------------------------------------------------------
+def add_usage_description() :
+    return " --aws-access-key-id=${AWS_ACCESS_KEY_ID} --aws-secret-access-key=${AWS_SECRET_ACCESS_KEY}"
+
+
+#--------------------------------------------------------------------------------------
+def add_parser_options( the_option_parser ) :
+    the_option_parser.add_option( "--aws-access-key-id",
+                                  metavar = "< Amazon key id >",
+                                  action = "store",
+                                  dest = "aws_access_key_id",
+                                  help = "(${AWS_ACCESS_KEY_ID}, by default)",
+                                  default = os.getenv( "AWS_ACCESS_KEY_ID" ) )
+    
+    the_option_parser.add_option( "--aws-secret-access-key",
+                                  metavar = "< Amazon secret key >",
+                                  action = "store",
+                                  dest = "aws_secret_access_key",
+                                  help = "(${AWS_SECRET_ACCESS_KEY}, by default)",
+                                  default = os.getenv( "AWS_SECRET_ACCESS_KEY" ) )
+    pass
+
+
+#--------------------------------------------------------------------------------------
+def extract_options( the_options ) :
+    AWS_ACCESS_KEY_ID = the_options.aws_access_key_id
+    if AWS_ACCESS_KEY_ID == None :
+        print_e( "Define AWS_ACCESS_KEY_ID parameter through '--aws-access-key-id' option\n" )
+        pass
+
+    AWS_SECRET_ACCESS_KEY = the_options.aws_secret_access_key
+    if AWS_SECRET_ACCESS_KEY == None :
+        print_e( "Define AWS_SECRET_ACCESS_KEY parameter through '--aws-secret-access-key' option\n" )
+        pass
+
+    return AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+
+
+#--------------------------------------------------------------------------------------
+def add_threading_usage_description() :
+    return " --number-threads=7"
+
+
+#--------------------------------------------------------------------------------------
+def add_threading_parser_options( the_option_parser ) :
+    the_option_parser.add_option( "--number-threads",
+                                  metavar = "< number of threads to use >",
+                                  type = "int",
+                                  action = "store",
+                                  dest = "number_threads",
+                                  help = "(\"%default\", by default)",
+                                  default = 8 )
+    pass
+
+
+#--------------------------------------------------------------------------------------
+def extract_threading_options( the_options, the_option_parser ) :
+    if the_options.number_threads < 1 :
+        the_option_parser.error( "'--number-threads' must be at least 1" )
+        pass
+
+    return the_options.number_threads
+
+
+#--------------------------------------------------------------------------------------
+def add_timeout_usage_description() :
+    return " --socket-timeout=3"
+
+
+#--------------------------------------------------------------------------------------
+def add_timeout_options( the_option_parser ) :
+    the_option_parser.add_option( "--socket-timeout",
+                                  metavar = "< socket timeout time >",
+                                  type = "int",
+                                  action = "store",
+                                  dest = "socket_timeout",
+                                  help = "(\"%default\", by default)",
+                                  default = None )
+    pass
+
+
+#--------------------------------------------------------------------------------------
+def extract_timeout_options( the_options, the_option_parser ) :
+    import socket
+    socket.setdefaulttimeout( the_options.socket_timeout )
+
+    return the_options.socket_timeout
+
+
+#--------------------------------------------------------------------------------------
+def wait_activation( the_instance, the_ssh_connect, the_ssh_client ) :
+    while True :
+        try :
+            print_d( '%s ' % the_instance.update() )
+            break
+        except :
+            continue
+        pass
+    
+    # Making sure that corresponding instances are ready to use
+    while True :
+        try :
+            if the_instance.update() == 'running' :
+                break
+            print_d( '.' )
+        except :
+            continue
+        pass
+    
+    print_d( ' %s\n' % the_instance.update() )
+
+    while True :
+        try :
+            the_ssh_connect()
+            ssh_command( the_ssh_client, 'echo  > /dev/null' )
+            break
+        except :
+            continue
+        pass
+
+    pass
+
+
+#------------------------------------------------------------------------------------------
+def remove_item( the_working_dir, the_file_item ) :
+    a_file_path = os.path.join( the_working_dir, the_file_item )
+    os.remove( a_file_path )
+
+    try :
+        os.rmdir( the_working_dir )
+    except :
+        pass
+
+    pass
+
+
+#------------------------------------------------------------------------------------------
+def upload_item( the_file_item, the_working_dir, the_file_bucket, the_printing_depth ) :
+    "Uploading file item"
+    a_file_path = os.path.join( the_working_dir, the_file_item )
+    print_d( "'%s'\n" % a_file_path, the_printing_depth )
+
+    a_part_key = Key( the_file_bucket )
+    a_part_key.key = the_file_item
+    a_part_key.set_contents_from_filename( a_file_path )
+    print_d( "%s\n" % a_part_key, the_printing_depth + 1 )
+
+    remove_item( the_working_dir, the_file_item )
+
+    pass
+
+
+#------------------------------------------------------------------------------------------
+class UploadItem :
+    def __init__( self, the_file_item, the_working_dir, the_file_bucket, the_printing_depth ) :
+        self.file_item = the_file_item
+        self.working_dir = the_working_dir
+        self.file_bucket = the_file_bucket
+        self.printing_depth = the_printing_depth
+        pass
+    
+    def run( self ) :
+        upload_item( self.file_item, self.working_dir, self.file_bucket, self.printing_depth )
+
+        return self
+
+    pass
+
+
+#------------------------------------------------------------------------------------------
+def upload_items( the_worker, the_file_bucket, the_working_dir, the_printing_depth ) :
+    "Uploading file items"
+
+    a_file_bucket_keys = [ an_item_key.key for an_item_key in the_file_bucket.get_all_keys() ]
+
+    a_dir_contents = os.listdir( the_working_dir )
+
+    a_dir_contents.sort()
+    a_dir_contents.reverse()
+
+    for a_file_item in a_dir_contents :
+        if a_file_item in a_file_bucket_keys :
+            remove_item( the_working_dir, a_file_item )
+
+            continue
+
+        a_task = UploadItem( a_file_item, the_working_dir, the_file_bucket, the_printing_depth + 1 )
+
+        the_worker.put( a_task )
+
+        pass
+
+    pass
+
+
+#--------------------------------------------------------------------------------------
