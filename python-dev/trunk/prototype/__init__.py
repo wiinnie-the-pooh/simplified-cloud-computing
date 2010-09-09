ae899814bf9afdf8e2c52a6278d7e8b6f2298670
alexander.a.borodin@gmail.com <alexander.a.borodin@gmail.com@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-09-09 21:39:02 +0000

    JSon responce for GetService

--- python-dev/trunk/prototype/__init__.py
+++ python-dev/trunk/prototype/__init__.py
@@ -3,50 +3,54 @@
 __version__ = '0.0.1'
 
 import httplib, urllib
+import json
 
 global _Connection
+global _Url
+global _Port
 
-def SCC_Connect()
-{
+#global configuration
+_Url = "cloud-mockup.appspot.com";
+_Port = 80;
+
+def home_config():
+	global _Url
+	global _Port
+	_Url = "localhost"
+	_Port = 8888
+
+def SCC_Connect():
 	global _Connection
-	_Connection = httplib.HTTPConnection('cloud-mockup.appspot.com/test', 80)
-	
+	_Connection = httplib.HTTPConnection(_Url, _Port)
+
+def SCC_GETService():
 	global _Connection
-	params = urllib.urlencode( {'UID': 'abd' } )
+	params = urllib.urlencode( {'UID': 'abd', 'Method' : 'GETService' } )
 	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 	_Connection.request("POST", "/test", params, headers)
 	response = _Connection.getresponse()
-	print response.status, response.reason
+	#print response.status, response.reason
 	print response.read()
-}
+	#jresponce = json.loads( response.read() ) # NVP: ("ServiceId",SERVICE_ID)
 
-def SCC_GETService()
-{	
-}
+def SCC_PUTBucket( aContainer ):
+	global _Connection
 
-def SCC_PUTBucket( aContainer )
-{
-}
+def SCC_GETBucket( aContainer ):
+	global _Connection
 
-def SCC_GETBucket( aContainer )
-{
-}
+def SCC_DELETEBucket( aContainer ):
+	global _Connection
 
-def SCC_DELETEBucket( aContainer )
-{
-}
+def SCC_PUTObject( anObject ):
+	global _Connection
 
-def SCC_PUTObject( anObject )
-{
-}
 
-def SCC_DELETEObject( anObject )
-{
-}
+def SCC_DELETEObject( anObject ):
+	global _Connection
 
-def SCC_GETObject( anObject )
-{
-}
+def SCC_GETObject( anObject ):
+	global _Connection
 
 
 
