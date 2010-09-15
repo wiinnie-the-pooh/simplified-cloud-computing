9a1696df98a11acb36c73d85195074b3f29dff12
alexey.petrov.nnov <alexey.petrov.nnov@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-09-15 16:57:55 +0000

    To advance and customize "ssh" related functionality   - to unable customization through the scripts or text files

--- cloud-agent/trunk/amazon_clean.py
+++ cloud-agent/trunk/amazon_clean.py
@@ -48,59 +48,68 @@ def stop_instance( the_instance ) :
 
 
 #--------------------------------------------------------------------------------------
+def delete_key_pair( the_key_pair ) :
+    print the_key_pair.name
+
+    # an_ec2_conn.delete_key_pair( the_key_pair ) # Does not work (bug)
+    the_key_pair.delete()
+
+    a_key_pair_dir = os.path.expanduser( "~/.ssh")
+    a_key_pair_file = os.path.join( a_key_pair_dir, the_key_pair.name ) + os.path.extsep + "pem"
+
+    if os.path.isfile( a_key_pair_file ) :
+        os.remove( a_key_pair_file )
+        pass
+    
+    pass
+
+
+#--------------------------------------------------------------------------------------
+def delete_security_group( the_ec2_conn, the_security_group ) :
+    if the_security_group.name != 'default' :
+        print the_security_group.name
+        the_ec2_conn.delete_security_group( the_security_group.name )
+        
+        pass
+    pass
+
+
+#--------------------------------------------------------------------------------------
+a_worker_pool = WorkerPool( 8 )
+
 for a_region in boto.ec2.regions( aws_access_key_id = AWS_ACCESS_KEY_ID, aws_secret_access_key = AWS_SECRET_ACCESS_KEY ) :
     an_ec2_conn = a_region.connect()
     print a_region
 
     print "-------------- Delete EC2 instances --------------"
     for a_reservation in an_ec2_conn.get_all_instances() :
-        print a_reservation
         for an_instance in a_reservation.instances :
+            # a_worker_pool.charge( stop_instance, ( an_instance ) )
             stop_instance( an_instance )
-
             pass
-
-        a_reservation.stop_all()
         pass
-
     print
-
+    a_worker_pool.join()
 
     print "-------------- Delete EC2 key pairs --------------"
     for a_key_pair in an_ec2_conn.get_all_key_pairs() :
-        print a_key_pair.name
-        # an_ec2_conn.delete_key_pair( a_key_pair ) # Does not work (bug)
-        a_key_pair.delete()
-
-        a_key_pair_dir = os.path.expanduser( "~/.ssh")
-        a_key_pair_file = os.path.join( a_key_pair_dir, a_key_pair.name ) + os.path.extsep + "pem"
-
-        if os.path.isfile( a_key_pair_file ) :
-            os.remove( a_key_pair_file )
-            
-            pass
-        
+        # a_worker_pool.charge( delete_key_pair, ( a_key_pair ) )
+        delete_key_pair( a_key_pair )
         pass
-
     print
 
 
     print "----------- Delete EC2 security groups -----------"
     for a_security_group in an_ec2_conn.get_all_security_groups() :
-        if a_security_group.name != 'default' :
-            print a_security_group.name
-            an_ec2_conn.delete_security_group( a_security_group.name )
-
-            pass
-
+        # a_worker_pool.charge( delete_security_group, ( an_ec2_conn, a_security_group ) )
+        delete_security_group( an_ec2_conn, a_security_group )
         pass
-
     print
 
+    a_worker_pool.join()
     pass
 
 
-#--------------------------------------------------------------------------------------
 print "---------------- Delete SQS queues ---------------"
 a_sqs_conn = boto.connect_sqs( AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY )
 for a_queue in a_sqs_conn.get_all_queues() :
@@ -112,8 +121,9 @@ for a_queue in a_sqs_conn.get_all_queues() :
 print
 
 
-#--------------------------------------------------------------------------------------
+a_worker_pool.shutdown()
+a_worker_pool.join()
+
+
 print "---------------------- OK ------------------------"
 
-
-#--------------------------------------------------------------------------------------
