7b40199020e5912a26a8c30c43be0174a6254e98
alexey.petrov.nnov <alexey.petrov.nnov@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-09-16 21:48:41 +0000

    to mark a stable release

---
+++ cloud-agent/tags/r0.24/amazon_run_ami.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+#--------------------------------------------------------------------------------------
+## Copyright 2010 Alexey Petrov
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+
+
+#--------------------------------------------------------------------------------------
+"""
+This script is responsible for the task packaging and sending it for execution in a cloud
+"""
+
+
+#--------------------------------------------------------------------------------------
+import balloon.common as common
+from balloon.common import print_d, print_e, sh_command, ssh_command, Timer
+
+from balloon import amazon
+from balloon.amazon import ec2 as amazon_ec2
+
+
+#--------------------------------------------------------------------------------------
+# Defining utility command-line interface
+
+an_usage_description = "%prog"
+an_usage_description += amazon_ec2.add_usage_description()
+an_usage_description += amazon.add_usage_description()
+an_usage_description += common.add_usage_description()
+
+from optparse import IndentedHelpFormatter
+a_help_formatter = IndentedHelpFormatter( width = 127 )
+
+from optparse import OptionParser
+a_option_parser = OptionParser( usage = an_usage_description, version="%prog 0.1", formatter = a_help_formatter )
+
+amazon_ec2.add_parser_options( a_option_parser )
+amazon.add_parser_options( a_option_parser )
+common.add_parser_options( a_option_parser )
+  
+ 
+#--------------------------------------------------------------------------------------
+# Extracting and verifying command-line arguments
+
+an_options, an_args = a_option_parser.parse_args()
+
+common.extract_options( an_options )
+
+AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY = amazon.extract_options( an_options )
+
+an_image_id, an_image_location, an_instance_type, a_min_count, a_max_count, a_ssh_host_port = amazon_ec2.extract_options( an_options )
+
+
+#--------------------------------------------------------------------------------------
+# Running actual functionality
+
+an_instance, a_key_pair_file = amazon_ec2.run_instance( an_image_id, an_image_location, an_instance_type, 
+                                                        a_min_count, a_max_count, a_ssh_host_port,
+                                                        AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY )
+print a_key_pair_file
+print an_instance.dns_name
+print a_ssh_host_port
+
+
+
+print_d( "\n-------------------------------------- OK ---------------------------------\n" )
+
