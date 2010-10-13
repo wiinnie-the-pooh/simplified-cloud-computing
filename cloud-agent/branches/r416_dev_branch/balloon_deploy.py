ef24680b190881998f795edc4bfc537e20955a6a
asimurzin <asimurzin@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-10-13 17:34:34 +0000

    to add install python-setuptools, python-dev to add sudo to ./setup.py sdist

--- cloud-agent/branches/r416_dev_branch/balloon_deploy.py
+++ cloud-agent/branches/r416_dev_branch/balloon_deploy.py
@@ -55,7 +55,8 @@ a_password, an_identity_file, a_host_port, a_login_name, a_host_name, a_command 
 
 print_d( "\n--------------------------- Canonical substitution ------------------------\n" )
 import sys
-an_engine = sys.argv[ 0 ]
+
+an_engine = sys.argv[ 1 ]
 
 a_call = "%s %s" % ( an_engine, ssh.compose_call( an_options ) )
 
@@ -64,17 +65,15 @@ ssh.print_call( a_password, an_identity_file, a_host_port, a_login_name, a_host_
 
 
 print_d( "\n----------------------- Running actual functionality ----------------------\n" )
-import os.path
-an_engine_dir = os.path.abspath( os.path.dirname( an_engine ) )
-sh_command( "cd %s && ./setup.py sdist" % an_engine_dir )
-
 a_ssh_client = ssh.connect( a_password, an_identity_file, a_host_port, a_login_name, a_host_name )
 
-ssh.command( a_ssh_client, ( "sudo apt-get -y install python-setuptools" ) )
-ssh.command( a_ssh_client, ( "sudo apt-get -y install python-all-dev" ) )
-
-a_working_dir = ssh.command( a_ssh_client, 'python -c "import os, os.path, tempfile; print tempfile.mkdtemp()"' )[ 0 ][ : -1 ]
+import os, tempfile
+a_working_dir = tempfile.mkdtemp()
 print_d( "a_working_dir = %s\n" % a_working_dir )
+
+import os.path
+an_engine_dir = os.path.abspath( os.path.dirname( an_engine ) )
+sh_command( "cd %s && sudo ./setup.py sdist" % an_engine_dir )
 
 import balloon
 a_balloon_name = "%s-%s" % ( balloon.NAME, balloon.VERSION )
@@ -82,14 +81,23 @@ a_balloon_archive_name = a_balloon_name + os.extsep + "tar.gz"
 a_balloon_source_archive = os.path.join( an_engine_dir, 'dist', a_balloon_archive_name )
 a_balloon_target_archive = os.path.join( a_working_dir, a_balloon_archive_name )
 
-# Uploading and installing into the cloud corresponding Python engine (itself)
+# Instantiating a sftp client
 a_sftp_client = a_ssh_client.open_sftp()
+
+ssh.command( a_ssh_client, 'mkdir %s' % a_working_dir )
+
+# Uploading and installing into the cloud corresponding Python engine (itself)
 a_sftp_client.put( a_balloon_source_archive, a_balloon_target_archive )
 ssh.command( a_ssh_client, 'cd %s && tar -xzf %s' % ( a_working_dir, a_balloon_archive_name ) )
 a_balloon_setup_dir = os.path.join( a_working_dir, a_balloon_name )
+ssh.command( a_ssh_client, 'sudo apt-get install -y python-dev' )
+ssh.command( a_ssh_client, 'sudo apt-get install -y python-setuptools' )
+
 ssh.command( a_ssh_client, 'cd %s && sudo python ./setup.py install' % ( a_balloon_setup_dir ) )
 
-# ssh.command( a_ssh_client, """python -c 'import shutil; shutil.rmtree( "%s" )'""" % a_working_dir )
+import shutil
+shutil.rmtree( a_working_dir ) # Cleaning temporal working folder
+
 a_ssh_client.close()
 
 
