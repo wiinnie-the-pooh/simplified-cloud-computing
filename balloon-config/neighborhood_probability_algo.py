961335de492029b56a1c531e68c725c2bdf3f0de
alexey.petrov.nnov@gmail.com <alexey.petrov.nnov@gmail.com@b8d0963c-52a6-e9c1-3c01-8968353061b8>
2010-11-16 07:46:51 +0000

    implementation of the new algorithm   - minor changes

--- balloon-config/neighborhood_probability_algo.py
+++ balloon-config/neighborhood_probability_algo.py
@@ -119,17 +119,21 @@ def sub_algo( the_x2y, the_cost, the_fun, the_start_x, the_end_x, the_probabilit
 
 
 #--------------------------------------------------------------------------------------
-def entry_point( the_fun, the_initial_x, the_finite_x, the_precision, the_count_attempts ) :
+def entry_point( the_fun, the_initial_x, the_finite_x, the_precision, the_nb_attempts ) :
     """ """
-    a_start_x = the_initial_x
-    an_end_x = the_finite_x
-    a_count_attempts = the_count_attempts
+    a_center_x = ( the_finite_x - the_initial_x ) / 2.0
+    a_region_x = ( the_finite_x - the_initial_x ) / 4.0
+
+    a_start_x = a_center_x - a_region_x / 2.0
+    an_end_x = a_center_x + a_region_x / 2.0
+
+    a_nb_attempts = the_nb_attempts
     a_cost = 0.0
     
     a_x2y = {}
     a_x = an_end_x
-    a_step = ( an_end_x - a_start_x ) / float( a_count_attempts )
-    for an_id in range( a_count_attempts + 1 ) :
+    a_step = ( an_end_x - a_start_x ) / float( a_nb_attempts )
+    for an_id in range( a_nb_attempts + 1 ) :
         an_y = the_fun( a_x )
         a_x2y[ a_x ] = an_y
         a_cost += a_x
@@ -137,22 +141,61 @@ def entry_point( the_fun, the_initial_x, the_finite_x, the_precision, the_count_
         pass
 
     print2_dict( a_x2y.keys(), a_x2y )
-
-    a_probability_interval = calc_probability_interval( a_x2y.keys(), a_x2y )
     print "cost : %4d\n" % a_cost
 
+    a_sub_nb_attempts = a_nb_attempts / 2
+    a_sub2_nb_attempts = a_sub_nb_attempts / 2
 
-    #-----------------------------------------------------------------------------------------
-    while ( 1.00 - a_probability_interval ) > float( the_precision / 100.0 )  :
-        a_x2y, a_cost, an_end_x, a_probability_interval = sub_algo( a_x2y, a_cost, the_fun, a_start_x, an_end_x, 
-                                                                    a_probability_interval, a_count_attempts )    
+    an_average_y = {}
+    a_xs = a_x2y.keys()
+    a_xs.sort()
+    for an_id in range( len( a_xs ) ) :
+        a_middle_x = a_x = a_xs[ an_id ]
+        an_y = a_x2y[ a_x ]
+
+        a_neighbor_nb_attemps = 0
+
+        print "[", 
+        if an_id < a_sub2_nb_attempts :
+            a_neighbor_nb_attemps += an_id
+            for a_sub_id in range( an_id ) :
+                a_x = a_xs[ a_sub_id ]
+                an_y += a_x2y[ a_x ]
+                print "%4d" % a_x,
+                pass
+            pass
+        else:
+            a_neighbor_nb_attemps += a_sub2_nb_attempts
+            for a_sub_id in range( an_id - a_sub2_nb_attempts, an_id ) :
+                a_x = a_xs[ a_sub_id ]
+                an_y += a_x2y[ a_x ]
+                print "%4d" % a_x,
+                pass
+            pass
+
+        print "| %4d |" % a_middle_x,
+
+        if a_nb_attempts - an_id < a_sub2_nb_attempts :
+            a_neighbor_nb_attemps += a_nb_attempts - an_id
+            for a_sub_id in range( an_id, a_nb_attempts ) :
+                a_x = a_xs[ a_sub_id ]
+                an_y += a_x2y[ a_x ]
+                print "%4d" % a_x,
+                pass
+            pass
+        else:
+            a_neighbor_nb_attemps += a_sub2_nb_attempts
+            for a_sub_id in range( an_id, an_id + a_sub2_nb_attempts ) :
+                a_x = a_xs[ a_sub_id ]
+                an_y += a_x2y[ a_x ]
+                print "%4d" % a_x,
+                pass
+            pass
+        print "]\n" 
+
         pass
 
-    an_optimize_x = an_end_x
-
-    print "solution : %4d / cost : %4d\n" % ( an_optimize_x, a_cost )
-
-    return an_optimize_x, a_cost
+    return None, a_cost
 
 
 #---------------------------------------------------------------------------------------------
